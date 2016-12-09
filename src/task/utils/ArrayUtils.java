af7cb11c56b5b7021a1650c9bf297067044aa615
fedorovalex <INOY310@yandex.ru>
2016-12-09 19:27:14 +0300

    Solution redone. Remarks corrected.

--- src/task/utils/ArrayUtils.java
+++ src/task/utils/ArrayUtils.java
@@ -2,35 +2,65 @@ package task.utils;
 
 import task.utils.predicate.Predicate;
 
-import java.util.Arrays;
+import java.util.Collection;
 
 public class ArrayUtils {
 
-    public static <T> T[] filter(T[] array, Predicate<? super T> predicate) {
-        if (array == null) {
-            throw new IllegalArgumentException("Массива нет.");
+    public static <T> void filter(Collection<? extends T> source, Collection<T> result, Predicate<? super T> predicate) {
+        if (source == null || result == null) {
+            throw new IllegalArgumentException("Коллекции нет.");
         }
-        if (array.length == 0) {
-            return array;
+        if (predicate == null) {
+            throw new IllegalArgumentException("Предиката нет.");
         }
-        T[] result = Arrays.copyOf(array, array.length);
-        int resultNumber = 0;
-        for (T arrayValue: array) {
+        result.clear();
+        for (T arrayValue: source) {
             if (predicate.checkCondition(arrayValue)) {
-                result[resultNumber] = arrayValue;
-                resultNumber++;
+                result.add(arrayValue);
             }
         }
-        return Arrays.copyOf(result, resultNumber);
     }
 
-    public static <T extends Comparable<T>> T findMiddleElement(T[] array) {
-        if (array == null || array.length == 0) {
-            throw new IllegalArgumentException("Массива нет или он пуст.");
+    public static <T extends Comparable<? super T>> T findMiddleElement(Collection<? extends T> collection) {
+        if (collection == null || collection.size() == 0) {
+            throw new IllegalArgumentException("Коллекции нет или она пуста.");
         }
-        T[] copyArray = Arrays.copyOf(array, array.length);
-        Arrays.sort(copyArray);
-        int middleIndex = (copyArray.length - 1) / 2;
-        return copyArray[middleIndex];
+
+        T middle = null;
+
+        for (T firstValue: collection) {
+            int amountElementsLess = 0;
+            int amountElementsOver = 0;
+            for (T secondValue: collection) {
+                if (firstValue.compareTo(secondValue) < 0) {
+                    amountElementsOver++;
+                }
+                if (firstValue.compareTo(secondValue) > 0) {
+                    amountElementsLess++;
+                }
+            }
+            if (isMiddle(amountElementsLess, amountElementsOver, collection.size()))
+            {
+                middle = firstValue;
+                break;
+            }
+        }
+        return middle;
+    }
+
+    private static boolean isMiddle(int amountElementsLess, int amountElementsOver, int amountElementsAll) {
+
+        int even = 0;
+        if (amountElementsAll % 2 == 0) {
+            even = 1;
+        }
+
+        if ((amountElementsOver == amountElementsAll / 2) || (amountElementsLess + even == amountElementsAll / 2)) {
+            return true;
+        }
+        if ((amountElementsOver < amountElementsAll / 2) && (amountElementsLess + even < amountElementsAll / 2)) {
+            return true;
+        }
+        return false;
     }
 }
--- src/test/ArrayUtilsTest.java
+++ src/test/ArrayUtilsTest.java
@@ -4,7 +4,9 @@ import org.junit.Test;
 import task.utils.ArrayUtils;
 import task.utils.predicate.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -12,55 +14,70 @@ public class ArrayUtilsTest {
 
     @Test
     public void TestFilterOnPredicateMoreHundred() {
-        Double[] source = {13.0, 2.5, 550.8, 2.4, 401.1, 5.0, 70.6, 5.7};
-        Double[] expectation = {550.8, 401.1};
+        List<Double> source = Arrays.asList(13.0, 2.5, 550.8, 2.4, 401.1, 5.0, 70.6, 5.7);
+        List<Number> expectation = Arrays.asList(550.8, 401.1);
         Predicate<Number> predicate = new MoreHundred<>();
-        assertTrue( Arrays.equals(expectation, ArrayUtils.<Double>filter(source, predicate)) );
 
+        List<Number> result = new ArrayList<>();
+        ArrayUtils.<Number>filter(source, result, predicate);
+
+        assertTrue( result.equals(expectation) );
     }
 
     @Test
     public void TestFilterOnPredicateEven() {
-        Integer[] source = {13, 2, 5, 2, 4, 5, 8, 5};
-        Integer[] expectation = {2, 2, 4, 8};
+        List<Integer> source = Arrays.asList(13, 2, 5, 2, 4, 5, 8, 5);
+        List<Integer> expectation = Arrays.asList(2, 2, 4, 8);
         Predicate<Number> predicate = new Even<>();
-        assertTrue( Arrays.equals(expectation, ArrayUtils.<Integer>filter(source, predicate)) );
+
+        List<Integer> result = new ArrayList<>();
+        ArrayUtils.<Integer>filter(source, result, predicate);
+
+        assertTrue( result.equals(expectation) );
     }
 
     @Test
     public void TestFilterOnPredicatePositive() {
-        Number[] source = {-13, -2, 5, 2, 4, -5, -8, 5, 0};
-        Number[] expectation = {5, 2, 4, 5};
+        List<Integer> source = Arrays.asList(-13, -2, 5, 2, 4, -5, -8, 5, 0);
+        List<Number> expectation = Arrays.asList(5, 2, 4, 5);
         Predicate<Number> predicate = new Positive<>();
-        assertTrue( Arrays.equals(expectation, ArrayUtils.<Number>filter(source, predicate)) );
+
+        List<Number> result = new ArrayList<>();
+        ArrayUtils.<Number>filter(source, result, predicate);
+
+        assertTrue( result.equals(expectation) );
     }
 
     @Test
     public void TestFilterOnPredicateLineLengthLimit() {
-        String[] source = {"123", "1234", "12345", "1", "12", "123456"};
-        String[] expectation = {"123", "1", "12"};
+        List<String> source = Arrays.asList("123", "1234", "12345", "1", "12", "123456");
+        List<String> expectation = Arrays.asList("123", "1", "12");
         int maxLength = 3;
         Predicate<String> predicate = new LineLengthLimit<>(maxLength);
-        assertTrue( Arrays.equals(expectation, ArrayUtils.<String>filter(source, predicate)) );
+
+        List<String> result = new ArrayList<>();
+        ArrayUtils.<String>filter(source, result, predicate);
+
+        assertTrue( result.equals(expectation) );
     }
 
     @Test
     public void TestFindMiddleElementWithIntegerType() {
-        Integer[] source = {13, 2, 6, 2, 4, 5, 8, 7};
+        List<Integer> source = Arrays.asList(13, 2, 6, 2, 4, 5, 8, 7);
         Integer expectation = 5;
         assertEquals(ArrayUtils.<Integer>findMiddleElement(source), expectation);
     }
 
     @Test
     public void TestFindMiddleElementWithDoubleType() {
-        Double[] source = {13.0, 2.0, 6.0, 2.0, 4.0, 5.0, 8.0, 7.0};
+        List<Double> source = Arrays.asList(13.0, 2.0, 6.0, 2.0, 4.0, 5.0, 8.0, 7.0);
         Double expectation = 5.0;
         assertEquals(ArrayUtils.<Double>findMiddleElement(source), expectation);
     }
 
     @Test
     public void TestFindMiddleElementWithStringType() {
-        String[] source = {"1", "2", "3", "4", "5", "6", "7"};
+        List<String> source = Arrays.asList("1", "2", "3", "4", "5", "6", "7");
         String expectation = "4";
         assertEquals(ArrayUtils.<String>findMiddleElement(source), expectation);
     }
